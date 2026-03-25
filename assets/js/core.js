d94ac9029849b9566a7508c9d8933ee3ac2b8061
Candido-mtz-2025 <alancandido350@gmail.com>
2026-03-25 11:44:19 -0300

    Remove console and alert statements from core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,5 +1,3 @@
-console.log("JS NOVO CARREGADO");
-alert("teste js novo");
 // ========================================
 // 🔥 CACHE DE DOM - OTIMIZAÇÃO V11.1
 // ========================================
